4ca1a47ce0b5e1a329004744a03e498dce38b2b2
Sebastian <sebgertz@gmail.com>
2019-05-21 16:53:07 -0400

    update styling, import Results component for image thumbnails

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, TextInput, TouchableHighlight, View} from 'react-native';
 import  Icon from 'react-native-vector-icons/FontAwesome';
+import Results from './src/components/Results';
 import { env } from './env.js'
 
 export default class App extends Component{
@@ -28,18 +29,21 @@ export default class App extends Component{
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Pixabay Image Viewer!</Text>
-        <TextInput 
-          style={styles.input}
-          placeholder={'Search for image'}
-          value={this.state.query}
-          onChangeText={query => this.setState({ query })}
-        />
-        <TouchableHighlight onPress = {()=>{this.fetchImages()}}>
-          <View>
-            <Icon name="search" size = {20} color = "#4285F4" />
-          </View>
-        </TouchableHighlight>
+        <View style={{alignItems: 'center'}}>
+          <Text style={styles.welcome}>Welcome to Pixabay Image Viewer!</Text>
+          <TextInput 
+            style={styles.input}
+            placeholder={'Search for image'}
+            value={this.state.query}
+            onChangeText={query => this.setState({ query })}
+          />
+          <TouchableHighlight onPress = {()=>{this.fetchImages()}}>
+            <View style={{marginTop: 20}}>
+              <Icon name="search" size = {25} color = "#4285F4" />
+            </View>
+          </TouchableHighlight>
+        </View>
+        {this.state.results.length !== 0 && <Results results={this.state.results}/>}
       </View>
     );
   }
@@ -49,16 +53,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    paddingTop: 30
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    marginTop: 50
   },
   input:{
-    height: '5%',
+    height: '10%',
     width: '50%',
     backgroundColor: 'white',
     borderRadius: 10,
