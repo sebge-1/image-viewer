1a31d4db91d22e0edb52ff71fd76a7a97addd641
Sebastian <sebgertz@gmail.com>
2019-05-22 14:58:39 -0400

    move AppContainer into components directory

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,11 +1,13 @@
 
 import { createStackNavigator, createAppContainer } from 'react-navigation';
-import Home from './screens/Home';
-import Results from './screens/Results';
+import Home from '../screens/Home';
+import Results from '../screens/Results';
+import DetailView from '../screens/DetailView';
 
 const AppNavigator = createStackNavigator({
   Home: { screen: Home },
-  Results: {screen: Results }
+  Results: {screen: Results },
+  DetailView: { screen: DetailView }
 });
 
 export const AppContainer = createAppContainer(AppNavigator);
