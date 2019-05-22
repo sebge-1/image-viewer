bddbf26c2f71f042c1024cd916a3b264c600aa5a
Sebastian <sebgertz@gmail.com>
2019-05-22 08:45:40 -0400

    refactor App to render only AppContainer

--- App.js
+++ App.js
@@ -8,30 +8,17 @@
 
 import React, {Component} from 'react';
 import { StyleSheet, Text, TextInput, TouchableHighlight, View} from 'react-native';
-import Results from './src/components/Results';
 import ResultsProvider from './src/components/ResultsProvider';
-import SearchButton from './src/components/SearchButton';
+import AppContainer from './src/AppContainer'
 
 export default class App extends Component{
-  state = {
-    query: ''
-  };
 
   render() {
+
     return (
       <ResultsProvider>
         <View style={styles.container}>
-          <View style={{alignItems: 'center'}}>
-            <Text style={styles.welcome}>Welcome to Pixabay Image Viewer!</Text>
-            <TextInput 
-              style={styles.input}
-              placeholder={'Search for image'}
-              value={this.state.query}
-              onChangeText={query => this.setState({ query })}
-            />
-            <SearchButton/>
-          </View>
-          <Results/>
+          <AppContainer/>
         </View>
       </ResultsProvider>
     );
@@ -44,17 +31,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#F5FCFF',
     paddingTop: 30
-  },
-  welcome: {
-    fontSize: 20,
-    marginTop: 50
-  },
-  input:{
-    height: '10%',
-    width: '50%',
-    backgroundColor: 'white',
-    borderRadius: 10,
-    paddingLeft: 10,
-    marginTop: 50
   }
 });
