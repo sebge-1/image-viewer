a975c93bdce62f64364dd2c102dd9cce06e8eadf
Sebastian <sebgertz@gmail.com>
2019-05-22 08:42:16 -0400

    add navigation to SearchButton

--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -4,11 +4,20 @@ import {ResultsContext} from '../contexts/ResultsContext';
 import  Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class SearchButton extends Component {
+
+  navigate = () => {
+    this.props.navigation.navigate('Results')
+  }
+
+  handlePress = (context) => {
+    this.navigate();
+    context.fetchImages();
+  }
   render() {
     return(
       <ResultsContext.Consumer> 
         {(context) => (
-          <TouchableHighlight onPress = {()=>{context.fetchImages()}}>
+          <TouchableHighlight onPress = {()=>{this.handlePress(context)}}>
               <View style={{marginTop: 20}}>
                 <Icon name="search" size = {25} color = "#4285F4" />
               </View>
