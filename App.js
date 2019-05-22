f5dbcfdc1326f97e529556a56b40ff67ba6ad316
Sebastian <sebgertz@gmail.com>
2019-05-21 22:47:12 -0400

    update App component after refactoring

--- App.js
+++ App.js
@@ -8,43 +8,32 @@
 
 import React, {Component} from 'react';
 import { StyleSheet, Text, TextInput, TouchableHighlight, View} from 'react-native';
-import  Icon from 'react-native-vector-icons/FontAwesome';
 import Results from './src/components/Results';
-import { env } from './env.js'
+import ResultsProvider from './src/components/ResultsProvider';
+import SearchButton from './src/components/SearchButton';
 
 export default class App extends Component{
   state = {
-    query: '',
-    results: []
+    query: ''
   };
-
-  fetchImages = () => {
-    const { BASE_URL, apiKeyPrepend, API_KEY, queryPrePend } = env;
-    const queryURL = encodeURI(BASE_URL + apiKeyPrepend + API_KEY + queryPrePend + this.state.query);
-    fetch(queryURL)
-      .then(response => response.json())
-      .then(data => this.setState({results: data.hits }));
-    };
 
   render() {
     return (
-      <View style={styles.container}>
-        <View style={{alignItems: 'center'}}>
-          <Text style={styles.welcome}>Welcome to Pixabay Image Viewer!</Text>
-          <TextInput 
-            style={styles.input}
-            placeholder={'Search for image'}
-            value={this.state.query}
-            onChangeText={query => this.setState({ query })}
-          />
-          <TouchableHighlight onPress = {()=>{this.fetchImages()}}>
-            <View style={{marginTop: 20}}>
-              <Icon name="search" size = {25} color = "#4285F4" />
-            </View>
-          </TouchableHighlight>
+      <ResultsProvider>
+        <View style={styles.container}>
+          <View style={{alignItems: 'center'}}>
+            <Text style={styles.welcome}>Welcome to Pixabay Image Viewer!</Text>
+            <TextInput 
+              style={styles.input}
+              placeholder={'Search for image'}
+              value={this.state.query}
+              onChangeText={query => this.setState({ query })}
+            />
+            <SearchButton/>
+          </View>
+          <Results/>
         </View>
-        {this.state.results.length !== 0 && <Results results={this.state.results}/>}
-      </View>
+      </ResultsProvider>
     );
   }
 }
