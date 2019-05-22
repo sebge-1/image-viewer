e73fa563a2edcc354d5ef787e150c0a043fb9e48
Sebastian <sebgertz@gmail.com>
2019-05-22 08:46:13 -0400

    set up Home screen

---
+++ src/screens/Home.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import SearchButton from '../components/SearchButton';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { ResultsContext } from '../contexts/ResultsContext';
+
+export default class Home extends React.Component {
+  render() {
+    return(
+      <ResultsContext.Consumer>
+        {(context) => (
+          <View style={{alignItems: 'center'}}>
+            <Text style={styles.welcome}>Welcome to Pixabay Image Viewer!</Text>
+            <TextInput 
+              style={styles.input}
+              placeholder={'Search for image'}
+              value={context.query}
+              onChangeText={query => context.updateQuery(query)}
+            />
+            <SearchButton navigation={this.props.navigation}/>
+          </View>
+        )}
+      </ResultsContext.Consumer>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  welcome: {
+    fontSize: 20,
+    marginTop: 50
+  },
+  input:{
+    height: '10%',
+    width: '50%',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    paddingLeft: 10,
+    marginTop: 50
+  }
+})
