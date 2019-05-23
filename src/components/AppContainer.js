5b6b0154ea4b1b5720f695d94304dac3f7a42eab
Sebastian <sebgertz@gmail.com>
2019-05-23 12:43:03 -0400

    set navigationOptions in AppContainer

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -5,9 +5,34 @@ import Results from '../screens/Results';
 import DetailView from '../screens/DetailView';
 
 const AppNavigator = createStackNavigator({
-  Home: { screen: Home },
-  Results: {screen: Results },
-  DetailView: { screen: DetailView }
+  Home: { 
+    screen: Home,
+    navigationOptions: {
+      header: null
+    } 
+  },
+  Results: { 
+    screen: Results,
+    navigationOptions: {
+      headerTitle: 'Search Again',
+      headerTintColor: 'white',
+      headerStyle: {
+        backgroundColor: '#282828'
+      }
+    }
+  }, 
+  DetailView: { 
+    screen: DetailView,
+    navigationOptions: {
+      headerTitle: 'Back',
+      headerTintColor: 'white',
+      headerStyle: {
+        backgroundColor: '#282828'
+      }
+    }
+  }
+}, {
+  initialRouteName: 'Home'
 });
 
 export const AppContainer = createAppContainer(AppNavigator);
