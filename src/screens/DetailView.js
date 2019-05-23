713a5ebe0a7482ae4e61e022a6f8134b81b39afd
Sebastian <sebgertz@gmail.com>
2019-05-23 12:44:54 -0400

    add Home button to DetailView header, minor stylistic changes

--- src/screens/DetailView.js
+++ src/screens/DetailView.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { AppContext } from '../contexts/AppContext'
 
 export default class DetailView extends React.Component {
+  
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerRight: (
+        <Icon name="home" size = {30} color = "#fff" style={{paddingRight: 20}} onPress={() => navigation.navigate('Home')} />
+      )
+    }
+  }
   render() {
     return(
       <AppContext.Consumer>
@@ -22,6 +31,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "black"
   },
   image: {
     minHeight: 300,
@@ -29,6 +40,7 @@ const styles = StyleSheet.create({
   },
   text: {
     marginTop: 50,
-    fontSize: 18
+    fontSize: 18,
+    color: 'white'
   }
 })
